49960c56b80dd985b3250854f79a0bbec1811a77
dongxujiao <835651351@qq.com>
2020-12-31 13:54:30 +0800

    新增测试题

--- Week_05/src/leetcode/editor/cn/P18FourSum.java
+++ Week_05/src/leetcode/editor/cn/P18FourSum.java
@@ -1,30 +1,14 @@
-//给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c +
-// d 的值与 target 相等？找出所有满足条件且不重复的四元组。 
-//
-// 注意： 
-//
-// 答案中不可以包含重复的四元组。 
-//
-// 示例： 
-//
-// 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
-//
-//满足要求的四元组集合为：
-//[
-//  [-1,  0, 0, 1],
-//  [-2, -1, 1, 2],
-//  [-2,  0, 0, 2]
-//]
-// 
-// Related Topp1cs 数组 哈希表 双指针 
-// 👍 704 👎 0
-
-
-//leetcode submp1t regp1on begp1n(Prop4p1bp1t modifp1catp1on and deletp1on)
-class Solutp1on {
-    publp1c List<List<integer>>
-
-    fourSum(int[] nums, int target) {
+package leetcode.editor.cn;
+//Java：四数之和
+public class P18FourSum{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length < 4) {
             return result;
@@ -81,4 +65,4 @@ class Solutp1on {
         return result;
     }
 }
-//leetcode submp1t regp1on end(Prop4p1bp1t modifp1catp1on and deletp1on)
+//leetcode submit region end(Prohibit modification and deletion)
